f6daab12054dd77e271cc8bf120514478c544be4
Code Sandbox <hello@codesandbox.io>
2021-03-25 19:46:07 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5b57d2abd9b1074e1b321356f9e3ff97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fadebe8b0127427d3f87b6efa8afff",
+    "url": "https://tumigsse.github.io/csb-hwl5d/index.html"
+  },
+  {
+    "revision": "58f84f9da8b90c0900eb",
+    "url": "https://tumigsse.github.io/csb-hwl5d/static/css/2.e9191fd6.chunk.css"
+  },
+  {
+    "revision": "4b70e3d75180479eb324",
+    "url": "https://tumigsse.github.io/csb-hwl5d/static/css/main.4620cbe1.chunk.css"
+  },
+  {
+    "revision": "58f84f9da8b90c0900eb",
+    "url": "https://tumigsse.github.io/csb-hwl5d/static/js/2.9b29ff3d.chunk.js"
+  },
+  {
+    "revision": "4b70e3d75180479eb324",
+    "url": "https://tumigsse.github.io/csb-hwl5d/static/js/main.f5430496.chunk.js"
+  },
+  {
+    "revision": "4058e7045e13b835ee2d",
+    "url": "https://tumigsse.github.io/csb-hwl5d/static/js/runtime~main.b1243212.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://tumigsse.github.io/csb-hwl5d/precache-manifest.5b57d2abd9b1074e1b321356f9e3ff97.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://tumigsse.github.io/csb-hwl5d/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
